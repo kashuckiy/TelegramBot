9637fee106f63f73fcd9e1a9e0e1f2945385061f
Nazar <nazar.rybchuk1999@gmail.com>
2020-11-01 02:50:26 +0200

    forward message

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const TOKEN = '788420263:AAEV8NT2xeljfJxlCG4L2WI825OpM6dqW-Q'
 
 console.log('Bot has been started...')
 
-
 const bot = new TelegramBot(TOKEN, {
     polling: {
         interval: 300,
@@ -15,22 +14,55 @@ const bot = new TelegramBot(TOKEN, {
     }
 })
 
-bot.on('inline_query', query =>{
+const inline_keyboard = [
+    [
+        {
+            text: 'Forward',
+            callback_data: 'forward'
+        },
+        {
+            text: 'Reply',
+            callback_data: 'reply'
+        }
+    ],
+    [
+        {
+            text: 'Edit',
+            callback_data: 'edit'
+        },
+        {
+            text: 'Delete',
+            callback_data: 'delete'
+        }
+    ]
+]
 
-    const results = []
+bot.on('callback_query', query => {
 
-    for(let i = 0; i < 5; i++){
-        results.push({
-           type: 'article',
-           id: i.toString(),
-           title: 'Title ' + i,
-           input_message_content:{
-            message_text:  `Article #${i+1}`
-           }
-        })
+    const { chat, message_id, text } = query.message
+
+    switch(query.data){
+        case 'forward':
+            //куда, откуда, что(ід того повідомлення які потрібно перенаправити)
+            bot.forwardMessage(chat.id, chat.id, message_id)//Перенаправлення повідомлень
+            break
     }
+    //Застаріла версія
+    // bot.answerCallbackQuery({
+    //     callback_query_id: query.id
+    // })
 
-    bot.answerInlineQuery(query.id, results, {
-        cache_time: 0
+    bot.answerCallbackQuery(query.id, {callback_query_id: query.id})
+    console.log(query)//log
+})
+
+bot.onText(/\/start/, (msg, [source, match]) => {
+    
+    const chatId  = msg.chat.id
+
+    bot.sendMessage(chatId, 'Keyboard', {
+        reply_markup:{
+            inline_keyboard
+        }
     })
 })
